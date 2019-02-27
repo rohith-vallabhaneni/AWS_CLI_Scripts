69d321268de09ef6c7094e65b66fbf093a0753dc
Nagaraj <rapellinagaraj@gmail.com>
2019-02-27 14:03:53 +0530

    Update changeWholeEc2Types.sh

--- changeWholeEc2Types.sh
+++ changeWholeEc2Types.sh
@@ -7,93 +7,97 @@
 check_desire_state(){
   current_instance_type=$(aws ec2 describe-instances --instance-id $instance_id --query "Reservations[*].Instances[*].InstanceType" --output text)
   if [ "$current_instance_type" = "$instance_type" ]; then
-    echo "INFO: Current and target instance types are same..."
-    echo "INFO: Skipping instance type change for $instance_id.."
-   # exit 0;
-     return 0;
-  else
-    echo "INFO: Identified change in instance type"
-     return 1;
+      echo "INFO: Current and target instance types are same..."
+      echo "INFO: Skipping instance type change for $instance_id.."
+      fla=1
+      start_instance
+  fi
+  if [ "$fla" != "1" ];
+  then
+      echo "INFO: Identified change in instance type"
+      stop_instance
+      create_snapshots
+      change_instance_type
+      start_instance
   fi
 }
 
-
 stop_instance () {
-    ##Code to stop instance, notify user, wait till instance has stopped before returning
-    ##First check if the instance is actually running
-    instance_state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
-    echo "INFO: Instance state is: $instance_state"
-            if [ $instance_state = "running" ]; then
-                    echo "INFO: Stopping instance $instance_id"
-                    aws ec2 stop-instances --instance-ids $instance_id
-            fi
-            i=0
-            state=$instance_state
-            until [ $state = "stopped" ]; do
-            sleep 1s
-            i=$((i+1))
-            state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
-            printf "."
-                    if [ "$i" -ge "180" ]; then
-                    printf "\n"
-                    echo "ERROR: Instance took too long to stop, exiting"
-                    exit 1
-                    fi
-            done
-            printf "\n"
-            echo "INFO: Instance successfully stopped after $i seconds"
+  ##Code to stop instance, notify user, wait till instance has stopped before returning
+  ##First check if the instance is actually running
+  instance_state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
+  echo "INFO: Instance state is: $instance_state"
+      if [ $instance_state = "running" ]; then
+          echo "INFO: Stopping instance $instance_id"
+          aws ec2 stop-instances --instance-ids $instance_id
+      fi
+      i=0
+      state=$instance_state
+      until [ $state = "stopped" ]; do
+          sleep 1s
+          i=$((i+1))
+          state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
+          printf "."
+          if [ "$i" -ge "180" ]; then
+              printf "\n"
+              echo "ERROR: Instance took too long to stop, exiting"
+              exit 1
+          fi
+      done
+      printf "\n"
+      echo "INFO: Instance successfully stopped after $i seconds"
     }
     create_snapshots(){
       aws ec2 describe-volumes --filter Name=attachment.instance-id,Values=$instance_id --query Volumes[*].{ID:VolumeId} --output text | tr '\t' '\n' > `pwd`/volumesList 2>&1
-                for volume_id in $(cat volumesList)
-                do
-               #Create a decription for the snapshot that describes the volume: servername.device-backup-date
-                temp="-backup-$(date +%Y-%m-%d)"
-                description=$instance_id"-"$volume_id$temp
-                        description=${description// /.}
-                        #echo "Volume ID is $volume_id" >> $logfile
-                        echo "INFO: Creating snapshot $description"
-                        #Take a snapshot of the current volume, and capture the resulting snapshot ID
-                        snapresult=$(aws ec2 create-snapshot --output=text --description $description --volume-id $volume_id --query SnapshotId)
-                # Add some tags to the snapshot
-                        tagresult=$(aws ec2 create-tags --resource $snapresult --tags Key=CreatedBy,Value=ChangeInstanceType)
-                        echo "INFO: Tags Added successfully.."
-                done
-        }
+      for volume_id in $(cat volumesList)
+      do
+         #Create a decription for the snapshot that describes the volume: servername.device-backup-date
+          temp="-backup-$(date +%Y-%m-%d)"
+          description=$instance_id"-"$volume_id$temp
+          description=${description// /.}
+          #echo "Volume ID is $volume_id" >> $logfile
+          echo "INFO: Creating snapshot $description"
+          #Take a snapshot of the current volume, and capture the resulting snapshot ID
+          snapresult=$(aws ec2 create-snapshot --output=text --description $description --volume-id $volume_id --query SnapshotId)
+          # Add some tags to the snapshot
+          tagresult=$(aws ec2 create-tags --resource $snapresult --tags Key=CreatedBy,Value=ChangeInstanceType)
+          echo "INFO: Tags Added successfully.."
+      done
+    }
     change_instance_type () {
            ##Code to change the instance type, notify user, return
-           instance_state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
-                   if [ $instance_state == "stopped" ]; then
-                           echo "INFO: Changing $instance_id to $instance_type"
-                           aws ec2 modify-instance-attribute --instance-id $instance_id --instance-type "{\"Value\": \"$instance_type\"}"
-                   else
-                           echo "ERROR: Instance state is: $instance_state"
-                           echo "ERROR: Cannot change instance type in this state, exiting" 1>&2
-                   exit 1
-                   fi
+      instance_state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
+          if [ $instance_state == "stopped" ]; then
+               echo "INFO: Changing $instance_id to $instance_type"
+               aws ec2 modify-instance-attribute --instance-id $instance_id --instance-type "{\"Value\": \"$instance_type\"}"
+          else
+               echo "ERROR: Instance state is: $instance_state"
+               echo "ERROR: Cannot change instance type in this state, exiting" 1>&2
+               exit 1
+          fi
    }
   ## Start Ec2 instance
     start_instance () {
-              ##Code to start instance, notify user, wait till instance is running before returning
-              echo "INFO: Starting instance $instance_id"
-              aws ec2 start-instances --instance-ids $instance_id
-              i=0
+      ##Code to start instance, notify user, wait till instance is running before returning
+      echo "INFO: Starting instance $instance_id"
+      aws ec2 start-instances --instance-ids $instance_id
+      i=0
+      state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
+      until [ $state = "running" ]; do
+              sleep 1s
+              i=$((i+1))
+              printf "."
+              if [ "$i" -ge "180" ]; then
+                    echo "ERROR: Instance took too long to start, exiting"
+                    exit 1
+              fi
               state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
-              until [ $state = "running" ]; do
-                      sleep 1s
-                      i=$((i+1))
-                      printf "."
-                      if [ "$i" -ge "180" ]; then
-                              echo "ERROR: Instance took too long to start, exiting"
-                              exit 1
-                      fi
-                      state=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].State.Name --output text)
               done
-              new_instance_type=$(aws ec2 describe-instance-attribute --instance-id $instance_id --attribute instanceType)
-              printf "\n"
-              echo "INSTANCEID $new_instance_type"
-              echo "INFO: Instance started in $i seconds"
-              echo "INFO: Completed."
+      new_instance_type=$(aws ec2 describe-instance-attribute --instance-id $instance_id --attribute instanceType)
+      printf "\n"
+      echo "INSTANCEID $new_instance_type"
+      echo "INFO: Instance started in $i seconds"
+      echo "INFO: Completed."
       }
 
 
@@ -105,18 +109,10 @@ change_instance (){
   a=0;
   length=$(( ${#instances_array[*]}-1 ))
   for var in `seq 0 2 $length`; do
-  echo "${instances_array[var]} ${instances_array[var+1]}";
-  instance_id="${instances_array[var]}"
-  instance_type="${instances_array[var+1]}"
-
-  if [ check_desire_state == 1 ]; then
-      stop_instance
-      create_snapshots
-      change_instance_type
-      start_instance
-  
-  else 
-      continue;
+     echo "${instances_array[var]} ${instances_array[var+1]}";
+     instance_id="${instances_array[var]}"
+     instance_type="${instances_array[var+1]}"
+     check_desire_state
   done
 }
 change_instance
